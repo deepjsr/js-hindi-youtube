fd05603fc595ed8194b61c626c5e0cf6fb873053
deepjsr <sarma.deepjyoti481@gmail.com>
2024-07-18 11:44:19 +0000

    caonversion of number type

---
+++ 01_basics/02_conversionOperation.js
@@ -0,0 +1,16 @@
+ let score=true;
+ console.log(typeof score);
+ console.log(typeof(score));
+
+ let valueInNumber=Number(score);  
+ console.log(typeof valueInNumber);
+ console.log(valueInNumber);
+
+//'33'=>33
+//'33thrsl'=>NaN
+let isBoolean='gf';
+let booeanIsLoggedIn= Boolean(isBoolean);
+console.log(booeanIsLoggedIn);
+//true=>1;false=0
+// ""=>false
+// "name"=>true
