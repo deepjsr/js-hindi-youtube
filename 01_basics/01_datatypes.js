3073d00cd93436e9fc20f5ee4ecf5bfc3b736d9e
deepjsr <sarma.deepjyoti481@gmail.com>
2024-07-18 13:02:01 +0000

    datatypes summery

--- 01_basics/01_datatypes.js
+++ 01_basics/01_datatypes.js
@@ -8,5 +8,23 @@
     // objects
 console.log(typeof undefined);//undefined
 console.log(typeof null);//object
+let bigInt=445869965566635566n
+
+console.log(typeof bigInt);
+
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
+
 
 
