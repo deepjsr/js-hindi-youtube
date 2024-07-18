29928a50e39aadb199d268a108a58a12610151f1
deepjsr <sarma.deepjyoti481@gmail.com>
2024-07-18 10:46:27 +0000

    basics

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,12 @@
+// Number=>2^55
+// bigint
+// String =>""
+// Boolean=>true or false
+// Undefined=> not defined yet
+// null=>Standalone value
+// Symbols=>
+    // objects
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
+
+
