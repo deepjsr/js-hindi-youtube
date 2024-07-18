69e50c7f91cd8727b03d45fd92998756e28dd6d0
deepjsr <sarma.deepjyoti481@gmail.com>
2024-07-18 12:22:29 +0000

    conversion final

--- 01_basics/02_conversionOperation.js
+++ 01_basics/02_conversionOperation.js
@@ -14,3 +14,19 @@ console.log(booeanIsLoggedIn);
 //true=>1;false=0
 // ""=>false
 // "name"=>true
+
+// **********************Operations*********************
+let val=3;
+let negVal=-3;
+// console.log(negVal);
+console.log(2**3);//'**' for power 
+let str='hello'
+let str2=' hitesh'
+// console.log(str + str2);
+// console.log('2'+ 2);
+// console.log(2+ '2');
+// console.log('2'+2+2);
+// console.log(2+2+'2');
+console.log(!true);
+console.log(+true);
+console.log(+'');
