6eeebe7abeb3b091432e768f2bdc6d1821e887cf
deepjsr <sarma.deepjyoti481@gmail.com>
2024-07-20 18:23:16 +0000

    scope lavel

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -6,3 +6,20 @@ function returnSecondValue(arrval) {
     return arrval[2];
 }
 console.log(returnSecondValue([34,88,'Kl raghul',809]));
+
+
+/********************Important*******************/
+const {PI,pow,floor}=Math;
+console.log(findRadius(5.35));
+function findRadius(_radius){
+    return floor(PI*pow(_radius,2));
+}
+// const radius=5.2;
+// const area=findRadius(radius)
+// console.log(`The radius of the circle with the radius ${radius} is : ${area.toFixed(1)}`);
+
+
+console.log(add(98));
+const add=function addOne(num1) {
+    return num1+1
+}
