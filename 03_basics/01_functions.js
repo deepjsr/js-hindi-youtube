e9efa9b7efcbf03bc0b9d1ff85ae85f65920af46
deepjsr <sarma.deepjyoti481@gmail.com>
2024-07-20 16:38:05 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,8 @@
+function calulatePrice(val1,val2,...num1){//'...' spread operator
+    return num1;
+}
+console.log(calulatePrice(55,96,200,299,99));
+function returnSecondValue(arrval) {
+    return arrval[2];
+}
+console.log(returnSecondValue([34,88,'Kl raghul',809]));
